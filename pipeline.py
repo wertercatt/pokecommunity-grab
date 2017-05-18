1fd97ff09d0dd976a2c007974a579ea05388ef53
wertercatt <wertcatt@gmail.com>
2017-05-17 21:40:38 -0500

    Don't forget to update VERSION.

--- pipeline.py
+++ pipeline.py
@@ -14,7 +14,7 @@ from seesaw.tracker import *
 
 DATA_DIR = "data"
 USER_AGENT = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27"
-VERSION = "20170517.03"
+VERSION = "20170517.04"
 
 class PrepareDirectories(SimpleTask):
   def __init__(self):
